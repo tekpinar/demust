7f0fcaee41a4b5c4003130b69d71c7cdeca9952e
Mustafa Tekpinar <tekpinar@buffalo.edu>
2022-07-15 14:40:47 +0200

    Changed the format of Spearman correlation p-value from float format to scientific format.

---
+++ demust/scripts/__init__.py
@@ -0,0 +1,9 @@
+"""
+Program Name: demust
+Author      : Mustafa TEKPINAR
+Copyright   : Mustafa TEKPINAR - 2022
+Email       : tekpinar@buffalo.edu
+Purpose     : A Python toolkit to modify, visualize and analyze deep mutational 
+              scanning data of proteins.
+"""
+
